67ee2d6cfb9a940dd3a0b6d4fed42db5869b0903
DaRos97 <dario.rossim@gmail.com>
2024-09-24 18:42:29 +0200

    Finished MF parameters at first round

--- classical_analysis/MF_parameters.py
+++ classical_analysis/MF_parameters.py
@@ -2,147 +2,63 @@ import numpy as np
 import sys, os
 import functions_MF as fs
 
-order = 'Neel'        #one of the clasical orders
+order = 'NonCoplanar2'        #one of the clasical orders
 
 print("Computing LRO ",order)
 
-if order == 'Coplanar':
-    print("alpha: ",fs.alpha_)
+if order[:-1] == 'Coplanar':
+    print("alpha: ",fs.alpha_,'\n')
+if order[:-1] == 'NonCoplanar':
+    print("theta: ",fs.theta_)
+    print("phi: ",fs.phi_,'\n')
 
+
+list_op_loop = [
+        ['a','alpha'],['b','alpha'],['c','alpha'],['d','alpha'],['e','alpha'],
+        ['a','beta'],['b','beta'],['c','beta1'],['c','beta2'],['d','beta1'],['d','beta2'],['e','beta'],
+        ['i','beta'],['j','beta'],['e','gamma'],['f','gamma'],
+        ]
 
 res = {}
-for type_op in ['alpha','beta']:
-    res[type_op] = {}
-    for loop in ['a','b','c','d','e']:
-        pr = 1 if (type_op=='beta' and loop=='e') else 0
-        res[type_op][loop] = fs.compute_loop(loop,type_op,order)
-        #
+for loop,type_op in list_op_loop:
+    if not type_op in res.keys():
+        res[type_op] = {}
+    pr = True if (type_op == 'beta1' and loop in ['c','d'] and 0) else False
+    res[type_op][loop] = fs.compute_loop(loop,type_op,order,pr)
+    #
+    if pr:
         print(loop,': ',type_op)
         print(res[type_op][loop])
+        input()
 
-print('Bh: ',fs.Bh(res))
-print('Bhp: ',fs.Bhp(res))
-print('Bt: ',fs.Bt(res))
-print('Btp: ',fs.Btp(res))
-print('Bd: ',fs.Bd(res))
 
-print('Ah: ',fs.Ah(res))
-print('Ahp: ',fs.Ahp(res))
-print('At: ',fs.At(res))
-print('Atp: ',fs.Atp(res))
-print('Ad: ',fs.Ad(res))
 
-exit()
+print('Ah: ',fs.Ah(res,order))
+print('Ahp: ',fs.Ahp(res,order))
+print('At: ',fs.At(res,order))
+print('Atp: ',fs.Atp(res,order))
+print('Ad: ',fs.Ad(res,order))
 
-phases_result = {}
-#Loop over all phases because we need all of them to compute the longer range ones
-list_all = ['a','b','A','B']
-for phi in Phis:
-    A = A_dic[phi]
-    Spins_in_loop = Spins_dic[order][phi]
-    ################
-    #First compute all the possible terms coming from the operators in the A-list
-    r = A[0]
-    for i1 in range(len(Spins_in_loop)-1):
-        temp = A[i1+1]
-        r2 = []
-        for i2 in range(len(r)):
-            for i3 in range(len(temp)):
-                if (r[i2][0] == '-' and temp[i3][0] == '-'):
-                    r2.append(r[i2][1:]+temp[i3][1:])
-                elif (r[i2][0] == '-' and temp[i3][0] in list_all):
-                    r2.append(r[i2]+temp[i3])
-                elif (temp[i3][0] == '-' and r[i2][0] in list_all):
-                    r2.append('-'+r[i2]+temp[i3][1:])
-                else:
-                    r2.append(r[i2]+temp[i3])
-        r = r2
-    #Now calculate their value one by one
-    Calculus = complex(0,0)
-    result = []
-    for num,res in enumerate(r):
-        temp_C = complex(1,0)
-        if res[0] == '-':       #remove the minus in front if it is there
-            res = res[1:]
-            result.append(str(num+1)+': -')
-            sign = -1
-        else:
-            result.append(str(num+1)+': ')
-            sign = 1
-        #Split the terms in the single multiplication
-        terms = res.split('*')
-        #For each term consider the spin sites one by one
-        for i in range(1,len(Spins)+1):        #6 for phi_A1'
-            temp = []
-            for t in terms:             #extract terms with same lattice position
-                if t[1] == str(i):
-                    temp.append(t)
-            #For each spin site compute the associated alfa, beta or gamma
-            if len(temp) > 2:#loops through i have more than two terms in the single product -> passes through 1 two times -> 4 terms
-                temp1 = [[temp[0],temp[3]],[temp[1],temp[2]]]
-                for temp_ in temp1:
-                    if (temp_[0][0] == 'A' and temp_[1][0] == 'a') or (temp_[0][0] == 'a' and temp_[1][0] == 'A'):          #alfa
-                        result[num] += 'a' + str(i)
-                        temp_C *= alfa(i-1,Spins)
-                    elif (temp_[0][0] == 'A' and temp_[1][0] == 'b') or (temp_[0][0] == 'b' and temp_[1][0] == 'A'):        #gamma
-                        result[num] += 'g' + str(i)
-                        temp_C *= gamma(i-1,Spins)
-                    elif (temp_[0][0] == 'B' and temp_[1][0] == 'a') or (temp_[0][0] == 'a' and temp_[1][0] == 'B'):        #gamma_
-                        result[num] += 'g_' + str(i)
-                        temp_C *= np.conjugate(gamma(i-1,Spins))
-                    elif (temp_[0][0] == 'B' and temp_[1][0] == 'b') or (temp_[0][0] == 'b' and temp_[1][0] == 'B'):        #beta
-                        result[num] += 'b' + str(i)
-                        temp_C *= beta(i-1,Spins)
-                    else:
-                        print("not recognized 1")
-                        exit()
-            else:
-                if (temp[0][0] == 'A' and temp[1][0] == 'a') or (temp[0][0] == 'a' and temp[1][0] == 'A'):
-                    result[num] += 'a' + str(i)
-                    temp_C *= alfa(i-1,Spins)
-                elif (temp[0][0] == 'A' and temp[1][0] == 'b') or (temp[0][0] == 'b' and temp[1][0] == 'A'):
-                    result[num] += 'g' + str(i)
-                    temp_C *= gamma(i-1,Spins)
-                elif (temp[0][0] == 'B' and temp[1][0] == 'a') or (temp[0][0] == 'a' and temp[1][0] == 'B'):
-                    result[num] += 'g_' + str(i)
-                    temp_C *= np.conjugate(gamma(i-1,Spins))
-                elif (temp[0][0] == 'B' and temp[1][0] == 'b') or (temp[0][0] == 'b' and temp[1][0] == 'B'):
-                    result[num] += 'b' + str(i)
-                    temp_C *= beta(i-1,Spins)
-                else:
-                    print("not recognized 2")
-                    exit()
-        #Sum the term to the result
-        Calculus += temp_C * sign
+print('-----')
 
-    if np.abs(Calculus) < 1e-10:
-        phases_result[phi] = np.nan
-        continue
+print('Bh: ',fs.Bh(res,order))
+print('Bhp: ',fs.Bhp(res,order))
+print('Bt: ',fs.Bt(res,order))
+print('Btp: ',fs.Btp(res,order))
+print('Bd: ',fs.Bd(res,order))
 
-    if phi == 'A1p':
-        phases_result[phi] = cmath.phase(Calculus) + np.pi
-    elif phi == 'A2':
-        phases_result[phi] = cmath.phase(Calculus) + phases_result['B1p'] - np.pi
-    elif phi == 'A2p':
-        phases_result[phi] = cmath.phase(Calculus) - phases_result['B1p'] - np.pi
-    elif phi == 'B2':
-        phases_result[phi] = cmath.phase(Calculus) + phases_result['A1p']
-    elif phi == 'B2p':
-        phases_result[phi] = -cmath.phase(Calculus) - phases_result['A1p']
-    elif phi == 'A3':
-        phases_result[phi] = cmath.phase(Calculus) + 2*phases_result['A1p'] - p1*np.pi
-    elif phi == 'B3':
-        phases_result[phi] = cmath.phase(Calculus) - phases_result['A1p'] - phases_result['B1p'] - p1*np.pi
-    elif phi in ['B1','B1p']:
-        phases_result[phi] = cmath.phase(Calculus)
+print('--------------------------------------')
 
-    if phases_result[phi] >= 4*np.pi:
-        phases_result[phi] -= 2*np.pi
-    if phases_result[phi] >= 2*np.pi:
-        phases_result[phi] -= 2*np.pi
-    if phases_result[phi] < -2*np.pi:
-        phases_result[phi] += 2*np.pi
-    if phases_result[phi] < 0:
-        phases_result[phi] += 2*np.pi
+print('phi_h: ',fs.phi_h(res,order))
+print('phi_hp: ',fs.phi_hp(res,order))
+print('phi_t: ',fs.phi_t(res,order))
+print('phi_tp: ',fs.phi_tp(res,order))
+print('phi_d: ',fs.phi_d(res,order))
 
-print('Result = ',phases_result)
+print('-----')
+
+print('psi_h: ',fs.psi_h(res,order))
+print('psi_hp: ',fs.psi_hp(res,order))
+print('psi_t: ',fs.psi_t(res,order))
+print('psi_tp: ',fs.psi_tp(res,order))
+print('psi_d: ',fs.psi_d(res,order))
--- classical_analysis/functions_MF.py
+++ classical_analysis/functions_MF.py
@@ -1,40 +1,101 @@
 import numpy as np
+import cmath
 
 def R_z(th):
     return np.array([[np.cos(th),-np.sin(th),0],[np.sin(th),np.cos(th),0],[0,0,1]])
 
 #Spins FM
 s1 = np.array([0,0,1/2])
-S_FM = {'a': [s1,s1,s1],'b': [s1,s1,s1],'c': [s1,s1,s1],'d': [s1,s1,s1],'e': [s1,s1,s1,s1,s1,s1]}
+S_FM = {'a': [s1,s1,s1],'b': [s1,s1,s1],'c': [s1,s1,s1],'d': [s1,s1,s1],'e': [s1,s1,s1,s1,s1,s1],'f':[s1,s1,s1,s1,s1,s1],'i':[s1,s1,s1,s1],'j':[s1,s1,s1,s1]}
 
 #Spins Neel
 s1 = np.array([0,0,1/2])
-S_Neel = {'a': [-s1,-s1,-s1],'b': [s1,s1,s1],'c': [-s1,s1,-s1],'d': [s1,s1,-s1],'e': [s1,-s1,s1,-s1,s1,-s1]}
+S_Neel = {'a': [-s1,-s1,-s1],'b': [s1,s1,s1],'c': [-s1,s1,-s1],'d': [s1,-s1,s1],'e': [s1,-s1,s1,-s1,s1,-s1],'f':[s1,-s1,s1,-s1,s1,-s1],'i':[-s1,s1,-s1,-s1],'j':[s1,s1,s1,-s1]}
 
 #Spins coplanar order
-alpha_ = np.pi/2
+alpha_ = np.pi/7+1
 s1 = []
 s2 = []
 for i in range(3):
     s1.append(np.matmul(R_z(2*np.pi/3*i),np.array([1/2,0,0])))
     s2.append(np.matmul(R_z(2*np.pi/3*i),np.array([np.cos(alpha_)/2,np.sin(alpha_)/2,0])))
 
-S_Coplanar = {'a': s2, 'b': [s1[0],s1[2],s1[1]], 'c': [s2[0],s1[0],s2[1]], 'd': [s1[0],s1[1],s2[1]], 'e': [s1[0],s2[0],s1[1],s2[1],s1[2],s2[2]]}
+S_Coplanar1 = {
+        'a': s2, 
+        'b': [s1[0],s1[2],s1[1]], 
+        'c': [s2[0],s1[0],s2[1]], 
+        'd': [s1[1],s2[1],s1[0]], 
+        'e': [s1[0],s2[0],s1[0],s2[0],s1[0],s2[0]],
+        'f': [s1[2],s2[0],s1[0],s2[1],s1[1],s2[2]],
+        'i': [s2[0],s1[0],s2[1],s2[2]],
+        'j': [s1[0],s1[2],s1[1],s2[1]]
+        }
+
+S_Coplanar2 = {
+        'a': [s2[0],s2[2],s2[1]], 
+        'b': [s1[0],s1[0],s1[0]], 
+        'c': [s2[0],s1[0],s2[2]], 
+        'd': [s1[0],s2[2],s1[0]], 
+        'e': [s1[0],s2[0],s1[1],s2[1],s1[2],s2[2]],
+        'f': [s1[1],s2[0],s1[0],s2[2],s1[2],s2[1]],
+        'i': [s2[0],s1[0],s2[2],s2[1]],
+        'j': [s1[0],s1[0],s1[0],s2[2]]
+        }
+
+#Spins non-coplanar1 order
+R_nc = np.array([[0,0,1],[1,0,0],[0,1,0]])
+flip_x = np.array([[1,0,0],[0,-1,0],[0,0,-1]])
+flip_y = np.array([[-1,0,0],[0,1,0],[0,0,-1]])
+flip_xy = np.array([[-1,0,0],[0,-1,0],[0,0,1]])
+theta_ = np.pi/7+0.5
+phi_ = np.pi/5+1
+s0 = np.array([np.sin(theta_)*np.cos(phi_),np.sin(theta_)*np.sin(phi_),np.cos(theta_)])/2
+s1 = []
+s1x = []
+s1y = []
+s1xy = []
+for i in range(3):
+    s1.append(np.matmul(np.linalg.matrix_power(R_nc,i),s0))
+    s1x.append(np.matmul(flip_x,s1[i]))
+    s1y.append(np.matmul(flip_y,s1[i]))
+    s1xy.append(np.matmul(flip_xy,s1[i]))
+
+S_NonCoplanar1 = {
+        'a': [s1[2],s1y[0],s1x[1]], 
+        'b': [s1[0],s1xy[1],s1y[2]], 
+        'c': [s1[2],s1[0],s1y[0]], 
+        'd': [s1y[2],s1y[0],s1[0]], 
+        'e': [s1[0],s1[2],s1[1],s1[0],s1[2],s1[1]],
+        'f': [s1x[2],s1[2],s1[0],s1y[0],s1y[1],s1x[1]],
+        'i': [s1[2],s1[0],s1y[0],s1x[1]],
+        'j': [s1[0],s1xy[1],s1y[2],s1y[0]]
+        }
+
+S_NonCoplanar2 = {
+        'a': [-s1[2],-s1y[0],-s1x[1]], 
+        'b': [s1[0],s1xy[1],s1y[2]], 
+        'c': [-s1[2],s1[0],-s1y[0]], 
+        'd': [s1y[2],-s1y[0],s1[0]], 
+        'e': [s1[0],-s1[2],s1[1],-s1[0],s1[2],-s1[1]],
+        'f': [s1x[2],-s1[2],s1[0],-s1y[0],s1y[1],-s1x[1]],
+        'i': [-s1[2],s1[0],-s1y[0],-s1x[1]],
+        'j': [s1[0],s1xy[1],s1y[2],-s1y[0]]
+        }
 
 ####
-S_order = {'FM': S_FM, 'Neel': S_Neel, 'Coplanar': S_Coplanar}
+S_order = {'FM': S_FM, 'Neel': S_Neel, 'Coplanar1': S_Coplanar1, 'Coplanar2': S_Coplanar2, 'NonCoplanar1': S_NonCoplanar1, 'NonCoplanar2': S_NonCoplanar2}
 
 
 #A^dag*A
 def ada(S):
     return complex(1/2 + S[2],0)
 def aad(S):
-    return complex(3/2 + S[2],0)
+    return complex(1/2 + S[2],0)
 #B^dag*B
 def bdb(S):
     return complex(1/2 - S[2],0)
 def bbd(S):
-    return complex(3/2 - S[2],0)
+    return complex(1/2 - S[2],0)
 #A^dag*B
 def adb(S):
     return complex(S[0],S[1])
@@ -52,7 +113,7 @@ def Adag(i,j,end=False):
     else:
         return [t1+'*',t2+'*']
 def A(i,j,end=False):
-    t1 = 'a'+str(i)+'*a'+str(j)
+    t1 = 'a'+str(i)+'*b'+str(j)
     t2 = '-b'+str(i)+'*a'+str(j)
     if end:
         return [t1,t2]
@@ -91,7 +152,22 @@ def beta(n):
         res.append(A(n,1,end=True))
     return res
 
-fun_operators = {'alpha': alpha, 'beta': beta}
+def gamma(n):
+    """
+    Operator loop type -> ad a b ad a b for n==6
+    """
+    res = []
+    for i in [1,4]:
+        res.append(Adag(i,i+1))
+        res.append(A(i+1,i+2))
+        if i == 4:
+            res.append(B(n,1,end=True))
+        else:
+            res.append(B(3,4))
+    return res
+
+
+fun_operators = {'alpha': alpha, 'beta': beta, 'gamma': gamma}
 
 def compute_loop(loop, type_op, order,pr=False):
     """
@@ -102,6 +178,10 @@ def compute_loop(loop, type_op, order,pr=False):
     """
     spins = S_order[order][loop]
     ls = len(spins)
+    if type_op in ['beta1','beta2','beta3']:
+        ind = int(type_op[-1])-1
+        type_op = 'beta'
+        spins = spins[ind:] + spins[:ind]
     op = fun_operators[type_op](len(spins))
     #Save in r all the products of a and b
     r = op[0]
@@ -131,87 +211,200 @@ def compute_loop(loop, type_op, order,pr=False):
         #Split the terms in the single multiplication
         terms = res.split('*')
         #For each term consider the spin sites one by one
+        val = []
         for i in range(1,ls+1):
             temp = ''
             for t in terms:             #extract terms with same lattice position
                 if t[-1] == str(i):
                     temp += t[:-1]
-            val = fun_s[temp](spins[i-1])
+            val.append(fun_s[temp](spins[i-1]))
             #For each spin site compute the associated ada, bdb, adb, ecc
-            temp_C *= val
+            temp_C *= val[-1]
         if pr:
-            print(temp_C)
             if not temp_C == 0:
                 print(res)
+            print(temp_C)
+            print(val)
+            print('-------------------------------------')
         #Sum the term to the result
         Calculus += temp_C * sign
     
     return Calculus/2**ls
 
-def Bh(res):
+def Bh(res,order):
     k1 = abs(res['alpha']['b'])
     k2 = abs(res['alpha']['a'])
     k3 = abs(res['alpha']['e'])
     k4 = abs(res['alpha']['c'])
     k5 = abs(res['alpha']['d'])
+    if k4 == 0 and k5 == 0:
+        return 0.
     return (k4/k5)**(1/2)*(k1*k3/k2)**(1/6)
-def Bhp(res):
+def Bhp(res,order):
     k1 = abs(res['alpha']['b'])
     k2 = abs(res['alpha']['a'])
     k3 = abs(res['alpha']['e'])
     k4 = abs(res['alpha']['c'])
     k5 = abs(res['alpha']['d'])
+    if k3 == 0:
+        return 0.
+    if k4 == 0 and k5 == 0:
+        return 0.
     return (k5/k4)**(1/2)*(k2*k3/k1)**(1/6)
-def Bt(res):
+def Bt(res,order):
     k1 = abs(res['alpha']['b'])
     return k1**(1/3)
-def Btp(res):
+def Btp(res,order):
     k2 = abs(res['alpha']['a'])
     return k2**(1/3)
-def Bd(res):
+def Bd(res,order):
     k1 = abs(res['alpha']['b'])
     k2 = abs(res['alpha']['a'])
     k3 = abs(res['alpha']['e'])
     k4 = abs(res['alpha']['c'])
     k5 = abs(res['alpha']['d'])
+    if k3 == 0:
+        return 0.
     return (k4*k5)**(1/2)*(k1*k3*k2)**(-1/6)
 
 
-def Ah(res):
+def Ah(res,order):
     l3 = abs(res['beta']['e'])
-    l4 = abs(res['beta']['c'])
-    l5 = abs(res['beta']['d'])
+    l4 = abs(res['beta1']['c'])
+    l5 = abs(res['beta1']['d'])
     if l3 == 0:
         return 0.
     if l4 == 0 and l5 == 0:
         return l3**(1/6)
-    return (Bt(res)*l4/Btp(res)/l5)**(1/2)*l3**(1/6)
-def Ahp(res):
+    return (Bt(res,order)*l4/Btp(res,order)/l5)**(1/2)*l3**(1/6)
+def Ahp(res,order):
     l3 = abs(res['beta']['e'])
-    l4 = abs(res['beta']['c'])
-    l5 = abs(res['beta']['d'])
+    l4 = abs(res['beta1']['c'])
+    l5 = abs(res['beta1']['d'])
     if l3 == 0:
         return 0.
     if l4 == 0 and l5 == 0:
         return l3**(1/6)
-    return (Btp(res)*l5/Bt(res)/l4)**(1/2)*l3**(1/6)
-def At(res):
+    return (Btp(res,order)*l5/Bt(res,order)/l4)**(1/2)*l3**(1/6)
+def At(res,order):
     l1 = abs(res['beta']['b'])
-    return (l1/Bt(res))**(1/2)
-def Atp(res):
+    return (l1/Bt(res,order))**(1/2)
+def Atp(res,order):
     l2 = abs(res['beta']['a'])
-    return (l2/Btp(res))**(1/2)
-def Ad(res):
+    return (l2/Btp(res,order))**(1/2)
+def Ad(res,order):
     l3 = abs(res['beta']['e'])
-    l4 = abs(res['beta']['c'])
-    l5 = abs(res['beta']['d'])
+    l4 = abs(res['beta1']['c'])
+    l5 = abs(res['beta1']['d'])
     if l5 == 0 and l4 == 0:
-        return 0
-    return (l4*l5/Bt(res)/Btp(res))**(1/2)*l3**(1/6)
+        return 0.
+    return (l4*l5/Bt(res,order)/Btp(res,order))**(1/2)*l3**(-1/6)
 
 
+# phi is phase of pairing A
+def phi_h(res,order):
+    if Ah(res,order) == 0:
+        return np.nan
+    return 0
+def phi_hp(res,order):
+    if Ahp(res,order) == 0:
+        return np.nan
+    if abs(res['beta']['i'])>0 and abs(res['beta']['j'])>0:
+        s10 = cmath.phase(res['beta']['i'])
+        s11 = cmath.phase(res['beta']['j'])
+        return (s10-s11+np.pi)%(2*np.pi)
+    else:
+        s4 = compute_loop('d','beta1',order)
+        if abs(s4)>0:
+            print('phi_hp at second order')
+            return (-cmath.phase(s4)+phi_d(res,order)-psi_t(res,order))%(2*np.pi)
+        else:
+            print('wtf')
+def phi_t(res,order):
+    if At(res,order) == 0:
+        return np.nan
+    if abs(res['beta']['i'])>0 and abs(res['beta2']['d'])>0 and abs(res['gamma']['e'])>0:
+        s5 = cmath.phase(res['beta2']['d'])
+        s10 = cmath.phase(res['beta']['i'])
+        r3 = cmath.phase(res['gamma']['e'])
+        return (s5+s10+np.pi+r3)%(2*np.pi)
+    else:
+        return 10
+def phi_tp(res,order):
+    if Atp(res,order) == 0:
+        return np.nan
+    if abs(res['beta']['i'])>0 and abs(res['beta2']['c'])>0:
+        s2 = cmath.phase(res['beta2']['c'])
+        s10 = cmath.phase(res['beta']['i'])
+        return (s2+s10)%(2*np.pi)
+    else:
+        return 10
+def phi_d(res,order):
+    if Ad(res,order) == 0:
+        return np.nan
+    if abs(res['beta']['i'])>0:
+        s10 = cmath.phase(res['beta']['i'])
+        return (s10+np.pi)%(2*np.pi)
+    else:
+        s1 = compute_loop('c','beta1',order)
+        if abs(s1)>0:
+            print('phi_d at second order')
+            return (cmath.phase(s1)+psi_tp(res,order))%(2*np.pi)
+        else:
+            print('wtf')
 
-
+# psi is phase of pairing A
+def psi_h(res,order):
+    if Bh(res,order) == 0:
+        return np.nan
+    return 0
+def psi_hp(res,order):
+    if Bhp(res,order) == 0:
+        return np.nan
+    if abs(res['gamma']['e'])>0:
+        r3 = cmath.phase(res['gamma']['e'])
+        return r3%(2*np.pi)
+    else:
+        g3 = compute_loop('e','alpha',order)
+        if abs(g3)>0:
+            print('psi_hp at second order')
+            return (cmath.phase(g3)/3)%(2*np.pi)
+        else:
+            print('wtf')
+def psi_t(res,order):
+    if Bt(res,order) == 0:
+        return np.nan
+    if abs(res['beta']['b'])>0:
+        r1 = cmath.phase(res['beta']['b'])
+        return r1%(2*np.pi)
+    else:
+        g1 = compute_loop('b','alpha',order)
+        if abs(g1)>0:
+            print('psi_t at second order')
+            return (cmath.phase(g1)/3)%(2*np.pi)
+        else:
+            print('wtf')
+def psi_tp(res,order):
+    if Btp(res,order) == 0:
+        return np.nan
+    if abs(res['beta']['a'])>0:
+        r2 = cmath.phase(res['beta']['a'])
+        return r2%(2*np.pi)
+    else:
+        g2 = compute_loop('a','alpha',order)
+        if abs(g2)>0:
+            print('psi_tp at second order')
+            return (cmath.phase(g2)/3)%(2*np.pi)
+        else:
+            print('wtf')
+def psi_d(res,order):
+    if Bd(res,order) == 0:
+        return np.nan
+    if abs(res['gamma']['f'])>0:
+        r5 = cmath.phase(res['gamma']['f'])
+        return r5%(2*np.pi)
+    else:
+        return 10
 
 
 
