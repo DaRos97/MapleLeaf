ce82277c03e7c40d93b67852f404105b6fd830fa
DaRos97 <dario.rossim@gmail.com>
2024-10-11 19:14:10 +0200

    finished copòlanar, missing non-coplanar

--- classical_analysis/4_spin_structure_factor.py
+++ classical_analysis/4_spin_structure_factor.py
@@ -5,11 +5,14 @@ and computing the ssf on it for each momentum point Q following the definition (
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import functions_ssf as fs
 import functions_cpd as fs_cpd
 from pathlib import Path
 from tqdm import tqdm
 import os,sys
+from time import time
+from datetime import timedelta
 
 """
 Chose the point to compute in the phase diagram
@@ -27,21 +30,28 @@ I = 0    #5#12#29      #over 51
 J = 25    #40#25#26     #over 51
 order = fs_cpd.name_list[nC][ind_order]
 print("Computing order ",order," at position Jd=","{:.4f}".format(Jds[I])," and Jt=","{:.4f}".format(Jts[J]))
+alpha = 5*np.pi/6
+print("alpha: ",alpha)
+UC = 30  #number of unit cells (6 sites) in each lattice direction (better to have it commensurate with the order we are computing)
+lattice = fs.lattice_functions[order](UC,alpha)
 
 """
 We start by defining the lattice
 """
-UC = 9  #number of unit cells (6 sites) in each lattice direction (better to have it commensurate with the order we are computing)
-nkx = 61   #number of k points in x,y
-nky = 61
+
+high_symm = 'b'
+factors = {'B':(7,10),'b':(6,4)}
+nkx, nky = fs.get_kpoints(factors[high_symm],150)
+print("Using ",UC,"x",UC," unit cells and ",nkx,"x",nky," momentum points")
 
 """
 Need to adapt the grid in momentum space to get the high symmetry points.
 """
-kxs = np.linspace(-np.linalg.norm(fs.b[0]),np.linalg.norm(fs.b[0]),nkx)
-kys = np.linspace(-np.linalg.norm(fs.b[0]),np.linalg.norm(fs.b[0]),nky)
-
-lattice = fs.lattice_functions[order](UC)
+vecx = fs.B_[1,0] if high_symm=='B' else fs.b_[1,0]
+vecy = fs.B_[1,1]/3 if high_symm=='B' else fs.b_[1,1]/3
+fx,fy = factors[high_symm]
+kxs = np.linspace(-vecx*fx,vecx*fx,nkx)
+kys = np.linspace(-vecy*fy,vecy*fy,nky)
 
 """
 Here we compute the ssf
@@ -56,28 +66,37 @@ UC_positions = np.array([   #in terms of t1,t2
 SSFzz_fn = fs.get_ssf_fn('zz',order,Jds[I],Jts[J],UC,nkx)
 SSFxy_fn = fs.get_ssf_fn('xy',order,Jds[I],Jts[J],UC,nkx)
 if not Path(SSFzz_fn).is_file():
-    SSFzz = np.zeros((nkx,nky),dtype=complex)
-    SSFxy = np.zeros((nkx,nky),dtype=complex)
+    time_initial = time()
+    SSFzz = np.zeros((nkx,nky))
+    SSFxy = np.zeros((nkx,nky))
     """
-    to parallelize this we need, for each k, a UC**2*6xUC**2*6 matrix of distances
+    To parallelize this we need, for each k, a UC**2*6 vector of distances (we fix one position to be (UC//2,UC//2,0))
     """
-    distances = np.zeros((UC**2*6,UC**2*6,2))
-    prod_zz = np.zeros((UC**2*6,UC**2*6))
-    prod_xy = np.zeros((UC**2*6,UC**2*6))
-    for i in range(UC**2*6):
-        iUCx, iUCy, iUC = (i//6//UC, i//6%UC, i%6)
-        for j in range(UC**2*6):
-            jUCx, jUCy, jUC = (j//6//UC, j//6%UC, j%6)
-            distances[i,j] = fs.T[0]*(iUCx-jUCx) + fs.T[1]*(iUCy-jUCy) + np.dot(fs.t.T,(UC_positions[iUC] - UC_positions[jUC]))
-            prod_zz[i,j] = lattice[iUCx,iUCy,iUC,2]*lattice[jUCx,jUCy,jUC,2]
-            prod_xy[i,j] = (lattice[iUCx,iUCy,iUC,0]*lattice[jUCx,jUCy,jUC,0]+lattice[iUCx,iUCy,iUC,1]*lattice[jUCx,jUCy,jUC,1])
-    for ik in tqdm(range(nkx*nky)):
-        ikx = ik//nky
-        iky = ik%nky
-        k_ = np.array([kxs[ikx],kys[iky]])
-        SSFzz[ikx,iky] = np.sum(np.ravel(np.exp(-1j*np.dot(distances,k_))*prod_zz))
-        SSFxy[ikx,iky] = np.sum(np.ravel(np.exp(-1j*np.dot(distances,k_))*prod_xy))
-    if 1:
+    fUCx = UC//2
+    fUCy = UC//2
+    #
+    distances = np.zeros((UC**2*6,2))
+    prod_zz = np.zeros(UC**2*6)
+    prod_xy = np.zeros(UC**2*6)
+    for fUC in range(6):
+        #Probably can be done faster
+        for i in range(UC**2*6):
+            iUCx, iUCy, iUC = (i//6//UC, i//6%UC, i%6)
+            distances[i] = fs.T_[0]*(iUCx-fUCx) + fs.T_[1]*(iUCy-fUCy) + np.dot(fs.t_.T,(UC_positions[iUC]-UC_positions[fUC]))
+            prod_zz[i] = lattice[iUCx,iUCy,iUC,2]*lattice[fUCx,fUCy,fUC,2]
+            prod_xy[i] = (lattice[iUCx,iUCy,iUC,0]*lattice[fUCx,fUCy,fUC,0]+lattice[iUCx,iUCy,iUC,1]*lattice[fUCx,fUCy,fUC,1])
+        """
+        We can create SSFzz in one shot by creating a matrix of kx,ky values and doing the dot product at once
+        """
+        X,Y = np.meshgrid(kxs,kys)
+        cos_kd = np.cos(np.einsum('ijm,km->ijk',np.dstack([X.T,Y.T]),distances))/2
+        SSFzz += np.sum(cos_kd*prod_zz, axis=2)
+        SSFxy += np.sum(cos_kd*prod_xy, axis=2)
+    SSFzz /= UC**2*6
+    SSFxy /= UC**2*6
+    time_final = timedelta(seconds=time()-time_initial)
+    print("SSF compute time: ",str(time_final))
+    if 0:
         np.save(SSFzz_fn,SSFzz)
         np.save(SSFxy_fn,SSFxy)
 else:
@@ -87,11 +106,27 @@ else:
 """
 Finally we plot the result
 """
-fig = plt.figure(figsize=(20,14))
-ax1 = fig.add_subplot(1,2,1)
-fs.plot_ssf(SSFzz,kxs,kys,ax1,'SSF zz')
-ax2 = fig.add_subplot(1,2,2)
-fs.plot_ssf(SSFxy,kxs,kys,ax2,'SSF xy')
+fig = plt.figure(figsize=(20,7))
+tt = ['zz','xy']
+X,Y = np.meshgrid(kxs,kys)
+for i in range(2):
+    data = SSFzz if i == 0 else SSFxy
+    ax = fig.add_subplot(1,2,i+1)
+    sc = ax.scatter(X.T,Y.T,c=data,
+                marker='s',
+                cmap=cm.plasma_r,
+                s=30,
+                norm=None #if i==0 else norm
+              )
+    norm = sc.norm
+    fs.plot_BZs(ax)
+    ax.set_xlim(kxs[0],kxs[-1])
+    ax.set_ylim(kys[0],kys[-1])
+    title = "SSF "+tt[i]
+    title += ", alpha="+"{:.2f}".format(alpha*180/np.pi)+"°" if order=='Coplanar' else ''
+    ax.set_title(title)
+    plt.colorbar(sc)
+    ax.set_aspect('equal')
 
 fig.tight_layout()
 plt.show()
--- classical_analysis/functions_ssf.py
+++ classical_analysis/functions_ssf.py
@@ -1,45 +1,117 @@
 import numpy as np
 
 def R_z(theta):
+    """
+    Just a rotaion around z.
+    """
     return np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+def R_z3(theta):
+    """
+    Just a rotaion around z.
+    """
+    return np.array([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]])
+def R_x3(theta):
+    return np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
+def R_y3(theta):
+    return np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0][-np.sin(theta),0,np.cos(theta)]])
+
+def get_reciprocal_vectors(a):
+    """
+    Get reciprocal lattice vectors of a1=a[0], a2=a[1]
+    """
+    a_r = np.zeros((2,2))
+    a_r[0] = 2*np.pi*np.array([a[1,1],-a[1,0]])/np.linalg.det(np.array([a[0],a[1]]))
+    a_r[1] = 2*np.pi*np.array([-a[0,1],a[0,0]])/np.linalg.det(np.array([a[0],a[1]]))
+    return a_r
+def angle_between_vectors(A, B):
+    angle_rad = np.arccos(np.clip(np.dot(A,B)/np.linalg.norm(A)/np.linalg.norm(B),-1.0,1.0))
+    return angle_rad
 
 """
 Latice vectors and reciprocal vectors
 """
-T = np.array([np.sqrt(7)*np.array([np.sqrt(3)/2,-1/2]),np.sqrt(7)*np.array([0,1])])
-B = np.zeros((2,2))
-B[0] = 2*np.pi*np.array([T[1,1],-T[1,0]])/np.linalg.det(np.array([T[0],T[1]]))
-B[1] = 2*np.pi*np.array([-T[0,1],T[0,0]])/np.linalg.det(np.array([T[0],T[1]]))
-t = np.array([1/np.sqrt(7)*np.array([np.sqrt(3),-2]),1/np.sqrt(7)*np.array([np.sqrt(3)*3/2,1/2])])
-b = np.zeros((2,2))
-b[0] = 2*np.pi*np.array([t[1,1],-t[1,0]])/np.linalg.det(np.array([t[0],t[1]]))
-b[1] = 2*np.pi*np.array([-t[0,1],t[0,0]])/np.linalg.det(np.array([t[0],t[1]]))
+T_ = np.array([np.sqrt(7)*np.array([np.sqrt(3)/2,-1/2]),np.sqrt(7)*np.array([0,1])])
+B_ = get_reciprocal_vectors(T_)
+#
+t_ = np.array([1/np.sqrt(7)*np.array([np.sqrt(3)*3/2,-1/2]),1/np.sqrt(7)*np.array([np.sqrt(3),2])])
+b_ = get_reciprocal_vectors(t_)
 """
 Vertices of BZ and extended BZ
 """
 vertices_BZ = np.zeros((6,2))
-vertices_BZ[0] = (B[0] + B[1])/3
+if abs(angle_between_vectors(B_[0],B_[1])-np.pi/3)<1e-3:
+    vertices_BZ[0] = (B_[0] + B_[1])/3
+elif abs(angle_between_vectors(B_[0],B_[1])-2*np.pi/3)<1e-3:
+    vertices_BZ[0] = (B_[0] - B_[1])/3
 for i in range(1,6):
     vertices_BZ[i] = np.matmul(R_z(np.pi/3*i),vertices_BZ[0])
 vertices_EBZ = np.zeros((6,2))
-vertices_EBZ[0] = (b[0] - b[1])/3
+if abs(angle_between_vectors(b_[0],b_[1])-np.pi/3)<1e-3:
+    vertices_EBZ[0] = (b_[0] + b_[1])/3
+elif abs(angle_between_vectors(b_[0],b_[1])-2*np.pi/3)<1e-3:
+    vertices_EBZ[0] = (b_[0] - b_[1])/3
 for i in range(1,6):
     vertices_EBZ[i] = np.matmul(R_z(np.pi/3*i),vertices_EBZ[0])
 """
 Lattices
 """
-def FM(UC):
+
+"""
+2 - 3 - 5
+ \ / \ /
+  1 - 4
+   \ /
+    0
+hexagonal links: 0-1,2-3,4-5
+triangular links: 1-3,3-4,4-1
+dimer links: 1-2,3-5,4-0
+"""
+def FM(UC,args):
     lattice = np.zeros((UC,UC,6,3))
     lattice[:,:,:,2] = 1/2
     return lattice
 
-def Neel(UC):
+def Neel(UC,args):
     lattice = np.zeros((UC,UC,6,3))
-    lattice[:,:,::2,2] = 1/2
-    lattice[:,:,1::2,2] = -1/2
+    lattice[:,:,[1,3,4],2] = 1/2
+    lattice[:,:,[0,2,5],2] = -1/2
     return lattice
 
-lattice_functions = {'FM':FM,'Neel':Neel}
+def coplanar(UC,alpha=np.pi/3):
+    lattice = np.zeros((UC,UC,6,3))
+    s1 = np.array([1,0,0])
+    s2 = np.matmul(R_z3(alpha),s1)
+    for ix in range(UC):
+        for iy in range(UC):
+            lattice[ix,iy,0] = np.matmul(R_z3(np.pi*2/3*(ix+iy)),s1)
+            lattice[ix,iy,2] = np.matmul(R_z3(np.pi*2/3*(ix+iy+1)),s1)
+            lattice[ix,iy,5] = np.matmul(R_z3(np.pi*2/3*(ix+iy+2)),s1)
+            #
+            lattice[ix,iy,1] = np.matmul(R_z3(np.pi*2/3*(ix+iy)),s2)
+            lattice[ix,iy,3] = np.matmul(R_z3(np.pi*2/3*(ix+iy+1)),s2)
+            lattice[ix,iy,4] = np.matmul(R_z3(np.pi*2/3*(ix+iy+2)),s2)
+    return lattice
+
+def noncoplanar_1(UC,theta=np.pi/3,phi=0):
+    lattice = np.zeros((UC,UC,6,3))
+    s1 = np.array([np.sin(theta)*np.cos(phi),np.sin(theta)*np.sin(phi),np.cos(theta)])
+    GR = np.array([[0,0,1],[1,0,0],[0,1,0]])
+    for ix in range(UC):
+        for iy in range(UC):
+            lattice[ix,iy,0] = np.matmul(R_y3(np.pi*iy),np.matmul(R_x3(np.pi*ix),s1))
+            lattice[ix,iy,1] = np.matmul(np.linalg.matrix_power(GR,2),lattice[ix,iy,0])
+            lattice[ix,iy,2] = np.matmul(R_y3(np.pi*iy+1),lattice[ix,iy,1])
+            lattice[ix,iy,3] = np.matmul(R_y3(np.pi*iy),np.matmul(,lattice[ix,iy,0]))
+            lattice[ix,iy,0] = np.matmul(R_x3(np.pi*ix),s1)
+            lattice[ix,iy,2] = np.matmul(R_z3(np.pi*2/3*(ix+iy+1)),s1)
+            lattice[ix,iy,5] = np.matmul(R_z3(np.pi*2/3*(ix+iy+2)),s1)
+            #
+            lattice[ix,iy,1] = np.matmul(R_z3(np.pi*2/3*(ix+iy)),s2)
+            lattice[ix,iy,3] = np.matmul(R_z3(np.pi*2/3*(ix+iy+1)),s2)
+            lattice[ix,iy,4] = np.matmul(R_z3(np.pi*2/3*(ix+iy+2)),s2)
+    return lattice
+
+lattice_functions = {'FM':FM,'Neel':Neel,'Coplanar':coplanar}
 
 def get_spec_txt(order,Jd,Jt,UC,nkx):
     return order+'_'+"{:.4f}".format(Jd)+"{:.4f}".format(Jt)+'_'+str(UC)+'_'+str(nkx)
@@ -47,14 +119,83 @@ def get_spec_txt(order,Jd,Jt,UC,nkx):
 def get_ssf_fn(direction,order,Jd,Jt,UC,nkx):
     return 'results/data_ssf/'+direction+'_'+get_spec_txt(order,Jd,Jt,UC,nkx)+'.npy'
 
-def plot_ssf(data,kx,ky,ax,title):
-    X,Y = np.meshgrid(kx,ky)
-    ax.scatter(X,Y,c=np.real(data),marker='s')
+def plot_BZs(ax):
     #hexagons
     for i in range(6):
-        ax.plot([vertices_BZ[i,0],vertices_BZ[(i+1)%6, 0]], [vertices_BZ[i,1],vertices_BZ[(i+1)%6, 1]], 'b-')
-        ax.plot([vertices_EBZ[i,0],vertices_EBZ[(i+1)%6, 0]], [vertices_EBZ[i,1],vertices_EBZ[(i+1)%6, 1]], 'b-')
+        ax.plot([vertices_BZ[i,0],vertices_BZ[(i+1)%6, 0]], [vertices_BZ[i,1],vertices_BZ[(i+1)%6, 1]],
+                color='k',
+                lw=2,
+                ls='--',
+                zorder=2,
+               )
+        ax.plot([vertices_EBZ[i,0],vertices_EBZ[(i+1)%6, 0]], [vertices_EBZ[i,1],vertices_EBZ[(i+1)%6, 1]],
+                color='k',
+                lw=2,
+                ls='--',
+                zorder=2,
+               )
 
-    ax.set_aspect('equal')
-    ax.set_title(title)
+def get_kpoints(factors,max_k=100):
+    res = []
+    for i in range(2):
+        list_ = [2*factors[i]+1,]
+        while True:
+            val = list_[-1]*2-1
+            if val < max_k:
+                list_.append(list_[-1]*2-1)
+            else:
+                res.append(list_[-1])
+                break
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
