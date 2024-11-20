a519de93f8769f31e53e2eab42d9b7bf7d42f85c
DaRos97 <dario.rossim@gmail.com>
2024-11-20 14:47:58 +0100

    Adjusted non-coplanar solution energy

--- classical_analysis/1_classical_phase_diagram.py
+++ classical_analysis/1_classical_phase_diagram.py
@@ -6,23 +6,18 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 plt.rcParams['text.usetex'] = True
 from matplotlib.lines import Line2D
+from matplotlib import cm
 import functions_cpd as fs
 import sys,os
 
 machine = fs.get_machine(os.getcwd())
 
 disp = 0#True if machine=='loc' else False
-
+plot_PD = 0
+plot_values = 0
 #
-plot_PD = 1
-plot_val = 0
-#what is this
-II = 43
-JJ = 0
-#
-
 Jh = 1
-nn = 9
+nn = 65
 bound = 4
 ng = 1  #0 or 1, gives the upper bound
 Jds = np.linspace(-bound,bound*ng,nn)
@@ -30,8 +25,6 @@ Jts = np.linspace(-bound,bound*ng,nn)
 
 res_fn = fs.get_res_cpd_fn(Jh,nn,Jds,Jts)
 
-txt_ansatz = ['kiwi','banana','mango']
-dic_ind_discrete = {'kiwi':8, 'banana':8, 'mango':2}
 if not Path(res_fn).is_file():
     Energies = np.zeros((nn,nn,3,8,4))  #Jd, Jt, one of the three ansatz, 8 max number of discrete pars, 4 results max for each solution (energy+3max continuum pars)
     for ind_d in range(nn):
@@ -41,10 +34,10 @@ if not Path(res_fn).is_file():
                 print("J_t = ",Jts[ind_t])
             Js = (Jh, Jds[ind_d], Jts[ind_t])
             for ind_ans in range(3):
-                txt = txt_ansatz[ind_ans]
+                txt = fs.txt_ansatz[ind_ans]
                 if disp:
                     print("Ansatz ",txt)
-                for ind_discrete in range(dic_ind_discrete[txt]):
+                for ind_discrete in range(fs.dic_ind_discrete[txt]):
                     args=(fs.get_discrete_index(ind_discrete,txt),Js)
                     if disp:
                         print("Discrete pars: ",args[0])
@@ -103,15 +96,11 @@ if plot_PD:#Plot PD
     marker = 's'
     for ind_d in range(nn):
         for ind_t in range(nn):
-            ind = np.unravel_index(np.argmin(Energies[ind_d,ind_t,:,:,0]),Energies[ind_d,ind_t,:,:,0].shape)
+#            ind = np.unravel_index(np.argmin(Energies[ind_d,ind_t,:,:,0]),Energies[ind_d,ind_t,:,:,0].shape)
+            minE = np.min(Energies[ind_d,ind_t,:,:,0])
+            ind = tuple(np.argwhere(abs(Energies[ind_d,ind_t,:,:,0]-minE)<1e-4)[0])
             if ind not in list_ind:
                 list_ind.append(ind)
-            if 0:   #check degeneracies
-                lind = np.argwhere(abs(Es[i,j,:,0]-Es[i,j,ind,0])<1e-4)
-                if len(lind)>1:
-                    ind = min(lind)[0]
-                if ind >= 6:
-                    ind = 6 + fs.get_which_NonCoplanar(Es[i,j,6,1:])
             ax.scatter(Jds[ind_d],Jts[ind_t],color=colors[ind[0],ind[1]],marker=marker,s=50)
 
     ax.set_xlabel(r"$J_d$",size=30)
@@ -119,24 +108,24 @@ if plot_PD:#Plot PD
     ax.xaxis.set_tick_params(labelsize=20)
     ax.yaxis.set_tick_params(labelsize=20)
 
-    if 1:   #legend
-        legend_entries = []
-        list_ind = np.array(list_ind)[np.argsort(np.array(list_ind)[:,0])]  #sort
-        for i in range(len(list_ind)):
-            ind = list_ind[i]
-            txt = txt_ansatz[ind[0]]
-            inds = fs.get_discrete_index(ind[1],txt)
-            txt += ', ('
-            for n in range(len(inds)):
-                txt += str(inds[n])
-                if not n==len(inds)-1:
-                    txt += ','
-            txt += ')'
-            legend_entries.append( Line2D([0], [0], marker='o', color='w', label=txt,
-                                  markerfacecolor=colors[ind[0],ind[1]], markersize=15)
-                                  )
+    #Legend
+    legend_entries = []
+    list_ind = np.array(list_ind)[np.argsort(np.array(list_ind)[:,0])]  #sort
+    for i in range(len(list_ind)):
+        ind = list_ind[i]
+        txt = fs.txt_ansatz[ind[0]]
+        inds = fs.get_discrete_index(ind[1],txt)
+        txt += ', ('
+        for n in range(len(inds)):
+            txt += str(inds[n])
+            if not n==len(inds)-1:
+                txt += ','
+        txt += ')'
+        legend_entries.append( Line2D([0], [0], marker='o', color='w', label=txt,
+                              markerfacecolor=colors[ind[0],ind[1]], markersize=15)
+                              )
 
-        ax.legend(handles=legend_entries,loc='lower right',fontsize=20)
+    ax.legend(handles=legend_entries,loc='lower right',fontsize=20)
 
     ax.set_title("Classical phase diagraf of $J_h-J_t-J_d$ maple leaf lattice",size=30)
 
@@ -146,55 +135,26 @@ if plot_PD:#Plot PD
     plt.show()
     exit()
 
-if print_txt:   #print values
-    i = II
-    j = JJ
-    print("Jd,Jt = ",Jds[i],Jts[j])
-    print("Energies: ",Es[i,j,:,0])
-    print("Order ",n_ord,":")
-    print(Es[i,j,n_ord,:])
 
-    args = (Jh,Jts[j],Jds[i])
-    exit()
-
-if plot_val: #Plot energies 
-    from matplotlib import cm
-    #
-    fig,ax = plt.subplots(subplot_kw={"projection":"3d"},nrows=1,ncols=4)
-    fig.set_size_inches(25,10)
-
-    nc = n_ord
-    nc = 6
-
+# Further checks
+if plot_values: #Plot energies and other stuff
     X,Y = np.meshgrid(Jds,Jts)
-    label = ['E',r'$\theta$',r'$\theta_p$',r'$\phi$',r'$\phi_p$']
+    ind_ans = 2
+    ind_discrete = 0
+    label = ['E',r'$\theta$']
+    if ind_ans ==1:
+        label.append(r'$\phi$')
+    if ind_ans == 2:
+        label.append(r'$\phi-\eta_R$')
 
-    ind = np.zeros((nn,nn),dtype=int)
-    for i in range(nn):
-        for j in range(nn):
-            ind[i,j] = np.argmin(Es[i,j,:,0])
-            lind = np.argwhere(abs(Es[i,j,:,0]-Es[i,j,ind[i,j],0])<1e-4)
-            if len(lind)>1:
-                ind[i,j] = min(lind)[0]
+    fig,ax = plt.subplots(subplot_kw={"projection":"3d"},nrows=1,ncols=len(label))
+    fig.set_size_inches(25,10)
     #Select values for which the nc order is GS
-    aa = np.argmin(Es[:,:,:,0],axis=2)
-    bb = np.where(ind!=nc)
-    if 1:
-        Es[bb[0],bb[1],nc,1] = np.nan   #Remove values where is not the GS
-        Es[bb[0],bb[1],nc,3] = np.nan   #Remove values where is not the GS
-        th = Es[:,:,nc,1]
-        ph = Es[:,:,nc,3]
-        R3 = np.array([[0,0,1],[1,0,0],[0,1,0]])
-        psi = np.zeros((nn,nn))
-        for i in range(nn):
-            for j in range(nn):
-                S1 = np.array([np.sin(th[i,j])*np.cos(ph[i,j]),np.sin(th[i,j])*np.sin(ph[i,j]),np.cos(th[i,j])])
-                psi[i,j] = np.degrees(np.arccos(np.clip(np.dot(S1,np.matmul(R3,S1)), -1.0, 1.0)))
-    for i in range(3):
-        Es[bb[0],bb[1],nc+i,0] = np.nan   #Remove values where is not the GS
-        Es[bb[0],bb[1],nc+i+1,0] = np.nan   #Remove values where is not the GS
-        #
-        ax[i].plot_surface(X,Y,abs(Es[:,:,nc+i,0].T-Es[:,:,nc+i+1,0].T),cmap=cm.coolwarm,linewidth=0,antialiased=False)
+    for i in range(len(label)):
+        if ind_ans == 2 and i==2:
+            ax[i].plot_surface(X,Y,(Energies[:,:,ind_ans,ind_discrete,2].T-Energies[:,:,ind_ans,ind_discrete,3].T)%np.pi,cmap=cm.coolwarm)
+        else:
+            ax[i].plot_surface(X,Y,Energies[:,:,ind_ans,ind_discrete,i].T,cmap=cm.coolwarm)
         ax[i].set_title(label[i])
         ax[i].set_xlabel(r'$J_d$')
         ax[i].set_ylabel(r'$J_t$')
--- classical_analysis/functions_cpd.py
+++ classical_analysis/functions_cpd.py
@@ -9,6 +9,19 @@ def E_kiwi(continuum_pars,*args):
     ep, n = discrete_pars
     return 3*(S(th)**2*(2*h*ep*C(n*np.pi/3)+2*t*C(2*n*np.pi/3)+(-1)**n*d*ep)+C(th)**2*(2*h*ep+2*t+d*ep))
 
+def E_kiwi2(continuum_pars,*args):
+    discrete_pars, Js = args
+    h,d,t = Js
+    th = continuum_pars
+    ep, n = discrete_pars
+    S = np.array([np.sin(th),0,np.cos(th)])
+    R = ep*np.array([[np.cos(n*np.pi/3),-np.sin(n*np.pi/3),0],[np.sin(n*np.pi/3),np.cos(n*np.pi/3),0],[0,0,1]])
+    T1 = T2 = np.identity(3)
+    Eh = 6*h*S@R@S
+    Et = 6*t*S@T1@T2@R@R@S
+    Ed = 3*d*S@T2@R@R@R@S
+    return Eh+Et+Ed
+
 def E_banana(continuum_pars,*args):
     discrete_pars, Js = args
     h,d,t = Js
@@ -16,12 +29,39 @@ def E_banana(continuum_pars,*args):
     ep, e1, e2 = discrete_pars
     return -3*d*(C(th)**2+S(th)**2*C(2*ph)) + 6*S(th)*(e2*S(ph)*C(th)*(h*ep+t)+(e1*e2*C(ph)*C(th)+e1*S(th)*C(ph)*S(ph))*(h*ep-t))
 
+def E_banana2(continuum_pars,*args):
+    discrete_pars, Js = args
+    h,d,t = Js
+    th, ph = continuum_pars
+    ep, e1, e2 = discrete_pars
+    S = np.array([np.sin(th)*np.cos(ph),np.sin(th)*np.sin(ph),np.cos(th)])
+    R = ep*np.array([[0,e1,0],[0,0,e2],[e1*e2,0,0]])
+    T1 = np.array([[1,0,0],[0,-1,0],[0,0,-1]])
+    T2 = np.array([[-1,0,0],[0,1,0],[0,0,-1]])
+    Eh = 6*h*S@R@S
+    Et = 6*t*S@T1@T2@R@R@S
+    Ed = 3*d*S@T2@R@R@R@S
+    return Eh+Et+Ed
+
 def E_mango(continuum_pars,*args):
     discrete_pars, Js = args
     h,d,t = Js
     th, ph, et = continuum_pars
     ep = discrete_pars[0]
     return 3*t*C(2*th) + 6*h*ep*(S(th)**2*(C(2*et)*C(2*ph)+S(2*et)*S(2*ph))-C(th)**2) - 3/2*d*ep*(C(th)**2+S(th)**2*(C(2*ph)*(C(2*et)+np.sqrt(3)*S(2*et))+S(2*ph)*(S(2*et)-np.sqrt(3)*C(2*et))))
+
+def E_mango2(continuum_pars,*args):
+    discrete_pars, Js = args
+    h,d,t = Js
+    th, ph, et = continuum_pars
+    ep = discrete_pars[0]
+    S = np.array([np.sin(th)*np.cos(ph),np.sin(th)*np.sin(ph),np.cos(th)])
+    R = ep*np.array([[np.cos(2*et),np.sin(2*et),0],[np.sin(2*et),-np.cos(2*et),0],[0,0,-1]])
+    T1 = T2 = 1/2*np.array([[-1,-np.sqrt(3),0],[np.sqrt(3),-1,0],[0,0,1]])
+    Eh = 6*h*S@R@S
+    Et = 6*t*S@T1@T2@R@R@S
+    Ed = 3*d*S@T2@R@R@R@S
+    return Eh+Et+Ed
 
 def get_discrete_index(ind_discrete,ans):
     if ans == 'kiwi':
@@ -31,146 +71,9 @@ def get_discrete_index(ind_discrete,ans):
     elif ans == 'mango':
         return ((-1)**(ind_discrete),)
 
-functions_fruit = {'kiwi':E_kiwi, 'banana':E_banana, 'mango':E_mango}
-
-def E_FM(*args):
-    h,t,d = args
-    return 6*h+6*t+3*d
-
-def E_Neel(*args):
-    h,t,d = args
-    return -6*h+6*t-3*d
-#
-def E_aC6(a,*args):
-    h,t,d = args
-    return np.sin(a)**2*(6*h+6*t+3*d)+3*np.cos(a)**2*(h-t-d)
-
-def E_aK6(a,*args): #K=\bar(C)
-    h,t,d = args
-    return np.sin(a)**2*(6*t-6*h-3*d)+3*np.cos(a)**2*(d-h-t)
-
-def E_aC3(a,*args):
-    h,t,d = args
-    return 6*np.sin(a)**2*(h+t)-3*np.cos(a)**2*(h+t)+3*d
-
-def E_aK3(a,*args):
-    h,t,d = args
-    return 6*np.sin(a)**2*(t-h)-3*np.cos(a)**2*(t-h)-3*d
-
-def E_aC2(a,*args):
-    h,t,d = args
-    return (6*h+3*d)*(np.sin(a)**2-np.cos(a)**2)+6*t
-
-def E_aK2(a,*args):
-    h,t,d = args
-    return (6*h+3*d)*(np.cos(a)**2-np.sin(a)**2)+6*t
-
-E1s = [E_aC6,E_aK6,E_aC3,E_aK3,E_aC2,E_aK2]
-
-def E_NC2(*args):
-    h,t,d = args
-    return -3*d
-
-def E_NK2(*args):
-    h,t,d = args
-    return 3*d
-
-E0s = [E_FM, E_Neel,E_NC2,E_NK2]
-
-def E_NC3(pars,*args):
-    tt,ph = pars
-    h,t,d = args
-    return 3*np.sin(tt)**2*(2*h*np.cos(2*ph) -t +d*np.cos(2*ph-2*np.pi/3)) - 3*np.cos(tt)**2*(2*h - 2*t + d)
-
-def E_NK3(pars,*args):
-    h,t,d = args
-    args2 = (-h,t,-d)
-    return E_NC3(pars,*args2)
-
-E2s = [E_NC3, E_NK3]
-
-def E_aA3(pars,*args):
-    tt, tp, ph = pars
-    h,t,d = args
-    return (6*h+3*d)*(S(tt)*S(tp)*C(ph)+C(tt)*C(tp)) + 6*t
-
-def E_bA3(pars,*args):
-    tt, tp, ph = pars
-    h,t,d = args
-    return 3*C(tt)*C(tp)*(2*h+d) + 3*S(tt)*S(tp)*C(ph)*(d-h) + 3/2*t*(3*C(tt)**2+3*C(tp)**2-2)
-
-def E_3NC9a(pars,*args):        #Coplanar
-    tt, tp, ph = pars
-    h,t,d = args
-    return 3*C(tt)*C(tp)*(2*h + d) + 3*S(tt)*S(tp)*( 2*h*C(ph) + d*C(ph-2*np.pi/3)) + 3/2*t*(3*C(tt)**2+3*C(tp)**2 -2)
-
-def E_3NC9b(pars,*args):
-    tt, tp, ph = pars
-    h,t,d = args
-    return 3*C(tt)*C(tp)*(2*h + d) + 3*S(tt)*S(tp)*(h*C(ph) + (h+d)*C(ph+2*np.pi/3)) + 3*t*(1 + C(tp)**2 - S(tp)**2/2)
-
-E3s = [E_aA3, E_bA3, E_3NC9a, E_3NC9b ]
-
-def E_3NC2a(pars,*args):
-    tt, tp, ph, pp = pars
-    h,t,d = args
-    return (3*h*(S(tt)*S(tp)*C(ph-pp) + C(tt)*C(tp) + S(tt)*C(tp)*S(ph) + C(tt)*S(tp)*C(pp) + S(tt)*S(tp)*C(ph)*S(pp)) +
-            3*t*(- S(tt)*C(tt)*C(ph) + S(tt)*C(tt)*S(ph) - S(tt)**2*S(ph)*C(ph) - S(tp)*C(tp)*C(pp) - S(tp)*C(tp)*S(pp) + S(tp)**2*S(pp)*C(pp)) + 
-            3*d*(S(tt)*S(tp)*S(ph)*C(pp) - S(tt)*C(tp)*C(ph) - C(tt)*S(tp)*S(pp)))
-
-def E_3NC2b(pars,*args):
-    tt, tp, ph, pp = pars
-    h,t,d = args
-    return (3*h*(S(tt)*S(tp)*C(ph-pp) + C(tt)*C(tp) - S(tt)*C(tp)*S(ph) - C(tt)*S(tp)*C(pp) + S(tt)*S(tp)*C(ph)*S(pp)) +
-            3*t*(S(tt)*C(tt)*C(ph) - S(tt)*C(tt)*S(ph) - S(tt)**2*S(ph)*C(ph) + S(tp)*C(tp)*C(pp) + S(tp)*C(tp)*S(pp) + S(tp)**2*S(pp)*C(pp)) + 
-            3*d*(S(tt)*S(tp)*S(ph)*C(pp) + S(tt)*C(tp)*C(ph) + C(tt)*S(tp)*S(pp)))
-
-def E_3NC2c(pars,*args):
-    tt, tp, ph, pp = pars
-    h,t,d = args
-    return (3*h*(S(tt)*S(tp)*C(ph-pp) + C(tt)*C(tp) + S(tt)*C(tp)*S(ph) - C(tt)*S(tp)*C(pp) - S(tt)*S(tp)*C(ph)*S(pp)) +
-            3*t*(S(tt)*C(tt)*C(ph) + S(tt)*C(tt)*S(ph) + S(tt)**2*S(ph)*C(ph) + S(tp)*C(tp)*C(pp) - S(tp)*C(tp)*S(pp) - S(tp)**2*S(pp)*C(pp)) + 
-            3*d*(-S(tt)*S(tp)*S(ph)*C(pp) + S(tt)*C(tp)*C(ph) - C(tt)*S(tp)*S(pp)))
-
-def E_3NC2d(pars,*args):
-    tt, tp, ph, pp = pars
-    h,t,d = args
-    return (3*h*(S(tt)*S(tp)*C(ph-pp) + C(tt)*C(tp) - S(tt)*C(tp)*S(ph) + C(tt)*S(tp)*C(pp) - S(tt)*S(tp)*C(ph)*S(pp)) +
-            3*t*(- S(tt)*C(tt)*C(ph) - S(tt)*C(tt)*S(ph) + S(tt)**2*S(ph)*C(ph) - S(tp)*C(tp)*C(pp) + S(tp)*C(tp)*S(pp) - S(tp)**2*S(pp)*C(pp)) + 
-            3*d*(-S(tt)*S(tp)*S(ph)*C(pp) - S(tt)*C(tp)*C(ph) + C(tt)*S(tp)*S(pp)))
-
-E4s = [E_3NC2a,E_3NC2b,E_3NC2c,E_3NC2d]
-
-
-Eall = {
-        '6': [E_FM, E_Neel, E_aC6,E_aK6,E_aC3,E_aK3,E_aC2,E_aK2, E_NC2,E_NK2,E_NC3,E_NK3],
-        '3': [E_FM, E_Neel, E_aA3, E_bA3, E_3NC9a, E_3NC9b, E_3NC2a],#,E_3NC2b,E_3NC2c,E_3NC2d],
-        'all': [E_FM, E_Neel, E_aA3, E_bA3, E_3NC9a, E_3NC9b, E_3NC2a,E_3NC2b,E_3NC2c,E_3NC2d, E_aC6,E_aK6,E_aC3,E_aK3,E_aC2,E_aK2, E_NC2,E_NK2,E_NC3,E_NK3],
-        }
-
-name_list = {
-        '6': ['FM','Neel','aC6','aK6','aC3','aK3','aC2','aK2','NC2','NK2','NC3','NK3'],
-        '3': ['FM','Neel','aA3','bA3','Coplanar','3NC9b','Non-Coplanar Ico','Non-Coplanar nIco 1','Non-Coplanar nIco 2'],#,'3NC2b','3NC2c','3NC2d'],
-        'all': ['FM','Neel','aA3','bA3','3NC9a','3NC9b','3NC2a','3NC2b','3NC2c','3NC2d','aC6','aK6','aC3','aK3','aC2','aK2','NC2','NK2','NC3','NK3']
-        }
-
-def get_which_NonCoplanar(pars):
-    th,tp,ph,pp = pars
-    R3 = np.array([[0,0,1],[1,0,0],[0,1,0]])
-    S1 = np.array([np.sin(th)*np.cos(ph),np.sin(th)*np.sin(ph),np.cos(th)])
-    S2 = np.array([np.sin(tp)*np.cos(pp),np.sin(tp)*np.sin(pp),np.cos(tp)])
-    diff = [np.matmul(np.linalg.matrix_power(R3,i),S1)-S2 for i in range(3)]
-    summ = [np.matmul(np.linalg.matrix_power(R3,i),S1)+S2 for i in range(3)]
-    #
-    for i in range(3):
-        if (abs(diff[i])<1e-3).all():
-            return 0
-        elif (abs(summ[i])<1e-3).all():
-            if np.degrees(np.arccos(np.clip(np.dot(S1,np.matmul(R3,S1)), -1.0, 1.0))) < 90:
-                return 1
-            else:
-                return 2
-    return 0
+functions_fruit = {'kiwi':E_kiwi2, 'banana':E_banana2, 'mango':E_mango2}
+txt_ansatz = ['kiwi','banana','mango']
+dic_ind_discrete = {'kiwi':8, 'banana':8, 'mango':2}
 
 def get_res_cpd_fn(Jh,nn,Jds,Jts):
     res_dn = 'results/data_cpd/'
